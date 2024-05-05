0043b853444614e922dee124af04968c1240e0ff
hyojin-park24 <gygy332@naver.com>
2024-05-05 18:41:34 +0900

    detail javascript code 적용

--- inflearn-js-1/script.js
+++ inflearn-js-1/script.js
@@ -4,8 +4,9 @@ const navigation = document.querySelector('.navigation');
 const menuList = document.querySelector('.menu-list');
 const menuItem = document.querySelector('.menu-item');
 
+
 const setElementContent = (element, content) => {
-    element.innerHTML = content;
+    element.innerText = content;
 };
 
 const setImageAttributes = (imangeElement, src, altText) => {
@@ -30,18 +31,20 @@ const getContent = ({name, imgUrl, description, price}) => {
 const renderMenuList = menu => {
     const renderList = menu.map(item => getContent(item));
 
-    menuList.replaceChildren(...renderList);
+     menuList.replaceChildren(...renderList);
+    // menuList.replaceChildren(renderList[0], renderList[1], renderList[2]);
 };
 
 const getRenderData = e => {
     const filter = e.target.id.replace('select-','');
     const renderData = 
-        filter == 'all' ? data : data.filter(item => item.type == filter);
+        filter == 'all' ? data : data.filter(item => item.type === filter);
 
     return renderData;
 };
 
 navigation.addEventListener('change', e => {
+    console.log(e)
     renderMenuList(getRenderData(e));
 });
 
